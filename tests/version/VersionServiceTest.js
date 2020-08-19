c6924d6e90232e503702178554661eac3a78d7de
nwjbrandon <nwjbrandon@gmail.com>
2020-08-19 13:46:59 +0800

    fix another test case

--- tests/version/VersionServiceTest.js
+++ tests/version/VersionServiceTest.js
@@ -3,6 +3,9 @@ import ControllerService from '../../src/Version/VersionService';
 
 describe('Test Version Service', () => {
   it('Should contain app version', () => {
-    Assert.equal(true, ControllerService.getVersion().version.includes('v1'));
+    Assert.equal(
+      true,
+      ControllerService.getVersion().version.includes('v0.0.1'),
+    );
   });
 });
