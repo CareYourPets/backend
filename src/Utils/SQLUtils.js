b4509e1f8da96cd7b985360d9115ffa4659c48ff
nwjbrandon <nwjbrandon@gmail.com>
2020-11-01 14:32:30 +0800

    fix bugs

--- src/Utils/SQLUtils.js
+++ src/Utils/SQLUtils.js
@@ -21,13 +21,13 @@ const SQLQueries = {
     );
   `,
   SELECT_CARE_TAKER: `
-    SELECT * FROM care_takers WHERE email=$1;
+    SELECT * FROM care_takers WHERE email=$1 AND is_deleted=false;
   `,
   SELECT_PET_OWNER: `
-    SELECT * FROM pet_owners WHERE email=$1;
+    SELECT * FROM pet_owners WHERE email=$1 AND is_deleted=false;
   `,
   SELECT_ADMINISTRATOR: `
-    SELECT * FROM psc_administrators WHERE email=$1;
+    SELECT * FROM psc_administrators WHERE email=$1 AND is_deleted=false;
   `,
   DELETE_CARE_TAKER: `
     UPDATE care_takers SET is_deleted=true WHERE email=$1;
