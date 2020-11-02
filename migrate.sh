8045dc732b043d05625e600d1351910bf11886e9
nwjbrandon <nwjbrandon@gmail.com>
2020-11-02 14:09:18 +0800

    feat: add seed data

--- migrate.sh
+++ migrate.sh
@@ -2,3 +2,4 @@
 PGPASSWORD=password psql -U postgres -h localhost -d dev -a -f setup/DropDB.sql
 PGPASSWORD=password psql -U postgres -h localhost -d dev -a -f setup/CreateDB.sql
 PGPASSWORD=password psql -U postgres -h localhost -d dev -a -f setup/SeedDB.sql
+yarn seed
--- start.sh
+++ start.sh
@@ -2,4 +2,5 @@
 psql -d $DATABASE_URL -a -f setup/DropDB.sql
 psql -d $DATABASE_URL -a -f setup/CreateDB.sql
 psql -d $DATABASE_URL -a -f setup/SeedDB.sql
+yarn seed
 yarn start
