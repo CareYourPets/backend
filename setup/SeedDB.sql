9aa423e81dc5edbb084ec13d2c8c0e50382196f9
nwjbrandon <nwjbrandon@gmail.com>
2020-11-02 14:15:23 +0800

    fix issues with sql

--- setup/SeedDB.sql
+++ setup/SeedDB.sql
@@ -1,186 +1,173 @@
-const bcrypt = require('bcrypt');
-const randomName = require('node-random-name');
-const randomSentence = require('random-sentence');
-const randomEmail = require('random-email');
-const randomMobile = require('random-mobile');
-const _ = require('lodash');
-const {Pool} = require('pg');
+INSERT INTO psc_administrators (
+	email,
+	password,
+	name,
+	gender,
+  contact,
+  location
+) VALUES (
+  'nwjbrandon@gmail.com', 
+  '$2b$10$OAOoB9h1ZVnakibXe9PFV.E.i1o3nF4j9STVugRvPdIFKWZWimr3.', 
+  'Brandon Ng',
+  'MALE',
+  '(+65)93221923',
+  '32-B Haw Par Village'
+);
 
-const pool = new Pool({
-  connectionString:
-    process.env.DATABASE_URL || 'postgresql://postgres:password@localhost/dev',
-});
+INSERT INTO psc_administrators (
+	email,
+	password,
+	name,
+	gender,
+  contact,
+  location
+) VALUES (
+  'ambhinav@yahoo.com', 
+  '$2b$10$OAOoB9h1ZVnakibXe9PFV.E.i1o3nF4j9STVugRvPdIFKWZWimr3.', 
+  'Brandon Ng',
+  'MALE',
+  '(+65)93221923',
+  '32-B Haw Par Village'
+);
 
-const salt = 10;
+INSERT INTO psc_administrators (
+	email,
+	password,
+	name,
+	gender,
+  contact,
+  location
+) VALUES (
+  'amir97azhar@gmail.com', 
+  '$2b$10$OAOoB9h1ZVnakibXe9PFV.E.i1o3nF4j9STVugRvPdIFKWZWimr3.', 
+  'Amir Azhar',
+  'MALE',
+  '(+65)93221923',
+  '32-B Haw Par Village'
+);
 
-const GENDERS = ['MALE', 'FEMALE'];
+INSERT INTO psc_administrators (
+	email,
+	password,
+	name,
+	gender,
+  contact,
+  location
+) VALUES (
+  'arunkumarr97@gmail.com', 
+  '$2b$10$OAOoB9h1ZVnakibXe9PFV.E.i1o3nF4j9STVugRvPdIFKWZWimr3.', 
+  'Arun Kumar',
+  'MALE',
+  '(+65)93221923',
+  '32-B Haw Par Village'
+);
 
-const AREAS = [
-  'NORTH',
-  'SOUTH',
-  'WEST',
-  'EAST',
-  'CENTRAL',
+INSERT INTO psc_administrators (
+	email,
+	password,
+	name,
+	gender,
+  contact,
+  location
+) VALUES (
+  'dragontho@gmail.com', 
+  '$2b$10$OAOoB9h1ZVnakibXe9PFV.E.i1o3nF4j9STVugRvPdIFKWZWimr3.', 
+  'Paul Tho',
+  'MALE',
+  '(+65)93221923',
+  '32-B Haw Par Village'
+);
+
+INSERT INTO pet_owners (
+  email,
+	password,
+	name,
+	gender,
+  contact,
+  area,
+  location,
+  bio
+) VALUES (
+  'test@example.com',
+  '$2b$10$OAOoB9h1ZVnakibXe9PFV.E.i1o3nF4j9STVugRvPdIFKWZWimr3.',
+  'Michael Scott',
+  'MALE',
+  '(+65)98765432',
   'NORTH-EAST',
-  'NORTH-WEST',
-  'SOUTH-EAST',
-  'SOUTH-WEST',
-];
+  'Hougang Ave 9, Blk 1, #02-1322, Singapore 123001',
+  'I am a little kid lover.'
+);
 
-const SQL = {
-  petOwner:
-    'INSERT INTO pet_owners (email, password, name, gender, contact, area, location, bio) VALUES ($1, $2, $3, $4, $5, $6, $7, $8)',
-  careTaker:
-    'INSERT INTO care_takers (email, password, name, gender, contact, area, location, bio) VALUES ($1, $2, $3, $4, $5, $6, $7, $8)',
-  administrator:
-    'INSERT INTO psc_administrators (email, password, name, gender, contact, area, location) VALUES ($1, $2, $3, $4, $5, $6, $7)',
-  petCategory:
-    'INSERT INTO pet_categories (category, base_price) VALUES ($1, $2)',
-};
+INSERT INTO care_takers (
+  email,
+	password,
+	name,
+	gender,
+  contact,
+  area,
+  location,
+  bio
+) VALUES (
+  'test@example.com',
+  '$2b$10$OAOoB9h1ZVnakibXe9PFV.E.i1o3nF4j9STVugRvPdIFKWZWimr3.',
+  'Dwight Schrute',
+  'MALE',
+  '(+65)98123456',
+  'NORTH-EAST',
+  'Hougang Ave 2, Blk 10, #05-14, Singapore 145010',
+  'Beets.'
+);
 
-const CATEGORIES = [
-  'Alpacas',
-  'Camels',
-  'Cats',
-  'Cattle',
-  'Dogs',
-  'Donkeys',
-  'Ferrets',
-  'Goats',
-  'Hedgehogs',
-  'Horses',
-  'Llamas',
-  'Pigs',
-  'Rabbits',
-  'Red foxes',
-  'Rodents',
-  'Sheep',
-  'Sugar gliders',
-  'Chicken',
-  'Companion parrots',
-  'Fowl',
-  'Columbines',
-  'Passerines',
-  'Goldfish',
-  'Koi',
-  'Siamese',
-  'Barb',
-  'Guppy',
-  'Molly',
-  'Mosquitofish',
-  'Oscar',
-  'Arthropods',
-  'Bees',
-  'Silk moth',
-];
+INSERT INTO care_takers (
+  email,
+	password,
+	name,
+	gender,
+  contact,
+  area,
+  location,
+  bio
+) VALUES (
+  'test2@example.com',
+  '$2b$10$OAOoB9h1ZVnakibXe9PFV.E.i1o3nF4j9STVugRvPdIFKWZWimr3.',
+  'Dwight Schrute',
+  'MALE',
+  '(+65)98123456',
+  'NORTH-EAST',
+  'Hougang Ave 2, Blk 10, #05-14, Singapore 145010',
+  'Beets.'
+);
 
-const HashPassword = async (password) => {
-  return bcrypt.hash(password, salt);
-};
+INSERT INTO pet_categories (
+  category,
+  base_price
+) VALUES (
+  'bulldog',
+  55.50
+), (
+  'snail',
+  10.90
+), (
+  'rabbit',
+  28.00
+);
 
-const petOwnerPayload = async (client) => {
-  const email = randomEmail();
-  const password = await HashPassword('password');
-  const name = randomName();
-  const gender = GENDERS[Math.floor(Math.random() * GENDERS.length)];
-  const contact = randomMobile({formatted: true});
-  const area = AREAS[Math.floor(Math.random() * AREAS.length)];
-  const location = randomSentence({min: 10, max: 15});
-  const bio = randomSentence({min: 100, max: 300});
-  client.query(SQL.petOwner, [
-    email,
-    password,
-    name,
-    gender,
-    contact,
-    area,
-    location,
-    bio,
-  ]);
-};
-const careTakerPayload = async (client) => {
-  const email = randomEmail();
-  const password = await HashPassword('password');
-  const name = randomName();
-  const gender = GENDERS[Math.floor(Math.random() * GENDERS.length)];
-  const contact = randomMobile({formatted: true});
-  const area = AREAS[Math.floor(Math.random() * AREAS.length)];
-  const location = randomSentence({min: 10, max: 15});
-  const bio = randomSentence({min: 100, max: 300});
-  client.query(SQL.careTaker, [
-    email,
-    password,
-    name,
-    gender,
-    contact,
-    area,
-    location,
-    bio,
-  ]);
-};
-
-const administratorPayload = async (client) => {
-  const email = randomEmail();
-  const password = await HashPassword('password');
-  const name = randomName();
-  const gender = GENDERS[Math.floor(Math.random() * GENDERS.length)];
-  const contact = randomMobile({formatted: true});
-  const area = AREAS[Math.floor(Math.random() * AREAS.length)];
-  const location = randomSentence({min: 10, max: 15});
-  client.query(SQL.administrator, [
-    email,
-    password,
-    name,
-    gender,
-    contact,
-    area,
-    location,
-  ]);
-};
-
-const categoryPayload = async (client, category) => {
-  const basePrice = (Math.random() * (0.12 - 0.02) + 0.02).toFixed(4) * 100;
-  client.query(SQL.petCategory, [category, basePrice]);
-};
-
-const main = async () => {
-  const client = await pool.connect();
-  try {
-    await client.query('BEGIN');
-    await Promise.all(
-      _.times(1000, async () => {
-        await petOwnerPayload(client);
-        
-      }),
-    );
-    await Promise.all(
-      _.times(1000, async () => {
-        await careTakerPayload(client);
-        
-      }),
-    );
-    await Promise.all(
-      _.times(10, async () => {
-        await administratorPayload(client);
-        
-      }),
-    );
-    await Promise.all(
-      _.map(CATEGORIES, async (category, _) => {
-        if (category === undefined) return;
-        await categoryPayload(client, category);
-        
-      }),
-    );
-    await client.query('COMMIT');
-  } catch (e) {
-    await client.query('ROLLBACK');
-    throw e;
-  } finally {
-    client.release();
-  }
-};
-
-(async () => {
-  await main();
-})();
+INSERT INTO pets (
+  name,
+  category,
+  email,
+  needs,
+  diet
+) VALUES (
+  'jackie',
+  'bulldog',
+  'test@example.com',
+  NULL,
+  'vegan'
+), (
+  'tommy',
+  'rabbit',
+  'test@example.com',
+  'He is blind.',
+  'cannibal'
+);
